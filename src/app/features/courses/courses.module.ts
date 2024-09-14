1efd2c63636c1ab3f56be5c28ae2b1fd2c14ef4e
Werostr <werost@student.agh.edu.pl>
2024-09-14 16:50:27 +0200

    add basic structure

---
+++ src/app/features/courses/courses.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "@app/shared/shared.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CoursesListModule } from "./courses-list/courses-list.module";
+
+// What is the purpose of this module?
+@NgModule({
+  imports: [CommonModule, SharedModule, FontAwesomeModule, CoursesListModule],
+  exports: [CoursesListModule],
+})
+export class CoursesModule {}
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent {
   constructor(library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
 
+  @Input() buttonText?: string;
+  @Input() iconName?: IconProp;
   // Use the names for the inputs `buttonText` and `iconName`.
 }
